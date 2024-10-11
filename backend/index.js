54254f561ea795941f3fcf81787c497315ddc2bf
NexaRise <156061608+NexaRise@users.noreply.github.com>
2024-10-10 18:52:39 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -291,6 +291,8 @@ const connectDB = require('./db');
 const Conversation = require('./models/conversationSchema'); // Import the model
 const path = require('path')
 const port = process.env.PORT || 4000;
+const url = `https://xenon-tkqt.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
 
 
 dotenv.config();
@@ -384,6 +386,26 @@ if (process.env.NODE_ENV === 'production') {
 
 
 // ------------------------------------------------------------ Deployment ---------------------------------------------------
+
+
+
+
+
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+
+setInterval(reloadWebsite, interval);
+
+
 
 
 app.listen(port, () => {
